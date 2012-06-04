9aa97cd81eb2609be2bd78d1048d6dc1066c52ee
Robert Pearce <robertwaltonpearce@gmail.com>
2012-06-04 17:31:44 -0400

    Parsing on each line

--- public/javascripts/application.coffee
+++ public/javascripts/application.coffee
@@ -1,13 +1,21 @@
 $(document).ready ->
   $('#new-haiku').live('keyup', ->
-    syllables = syllableCount($(this).val())
-    $('.syllable-count').html(syllables)
+    [_first, _second, _third] = $(this).val().split('\n');
+    first_line_syllables      = syllableCount(_first)
+    second_line_syllables     = syllableCount(_second)
+    third_line_syllables      = syllableCount(_third)
+    total_syllables           = first_line_syllables + second_line_syllables + third_line_syllables
+
+    $('.syllable-count').html(total_syllables)
   )
 
   syllableCount = (word) ->
-    word = word.toLowerCase()
-    if word is '' or word is null then return 0
-    if word.length <= 3 and word.length > 0 then return 1
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
-    word = word.replace(/^y/, '')
-    return word.match(/[aeiouy]{1,2}/g).length
+    unless word is undefined
+      word = word.toLowerCase()
+      if word is '' or word is null then return 0
+      if word.length <= 3 and word.length > 0 then return 1
+      word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+      word = word.replace(/^y/, '')
+      return word.match(/[aeiouy]{1,2}/g).length
+    else
+      return 0
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,17 +3,25 @@
   $(document).ready(function() {
     var syllableCount;
     $('#new-haiku').live('keyup', function() {
-      var syllables;
-      syllables = syllableCount($(this).val());
-      return $('.syllable-count').html(syllables);
+      var first_line_syllables, second_line_syllables, third_line_syllables, total_syllables, _first, _ref, _second, _third;
+      _ref = $(this).val().split('\n'), _first = _ref[0], _second = _ref[1], _third = _ref[2];
+      first_line_syllables = syllableCount(_first);
+      second_line_syllables = syllableCount(_second);
+      third_line_syllables = syllableCount(_third);
+      total_syllables = first_line_syllables + second_line_syllables + third_line_syllables;
+      return $('.syllable-count').html(total_syllables);
     });
     return syllableCount = function(word) {
-      word = word.toLowerCase();
-      if (word === '' || word === null) return 0;
-      if (word.length <= 3 && word.length > 0) return 1;
-      word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-      word = word.replace(/^y/, '');
-      return word.match(/[aeiouy]{1,2}/g).length;
+      if (word !== void 0) {
+        word = word.toLowerCase();
+        if (word === '' || word === null) return 0;
+        if (word.length <= 3 && word.length > 0) return 1;
+        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+        word = word.replace(/^y/, '');
+        return word.match(/[aeiouy]{1,2}/g).length;
+      } else {
+        return 0;
+      }
     };
   });
 
